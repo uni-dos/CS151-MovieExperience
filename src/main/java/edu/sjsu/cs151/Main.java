dadcba9d1d7c785c10c7eea89a615537aa065a0d
uni-dos <uni.dos12@outlook.com>
2023-11-12 11:00:54 -0800

    manually refactored both code bases

--- src/main/java/edu/sjsu/cs151/Main.java
+++ src/main/java/edu/sjsu/cs151/Main.java
@@ -7,12 +7,12 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-
+// test class to see if networking works
 public class Main {
     public static void main(String[] args) {
         MovieRequest connection = RetrofitInstance.getRetrofitInstance();
 
-
+        MovieExperienceApplication.main(args);
         Call<Movie> m = connection.getMovieData("M3GAN", null);
 
         m.enqueue(new Callback<>() {
@@ -35,11 +35,36 @@ public class Main {
 
             }
 
+
             @Override
             public void onFailure(Call<Movie> call, Throwable throwable) {
                 System.out.println(throwable.getMessage());
             }
         });
+
+        connection.getMovieData("Ant-man", "2023").enqueue(new Callback<Movie>() {
+            @Override
+            public void onResponse(Call<Movie> call, Response<Movie> response) {
+                if (response.isSuccessful()) {
+                    Movie movie = response.body();
+
+                    System.out.println(movie.getTitle());
+                    System.out.println(movie.getRated());
+                    System.out.println(movie.getPosterURL());
+                    System.out.println(movie.getGenre());
+                    System.out.println(movie.getPlot());
+
+                } else {
+                    System.out.println(response.errorBody());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Movie> call, Throwable throwable) {
+
+            }
+        });
+
 
 
     }
--- src/main/java/edu/sjsu/cs151/MovieExperienceApplication.java
+++ src/main/java/edu/sjsu/cs151/MovieExperienceApplication.java
@@ -1,4 +1,4 @@
-package com.example.movieexperiencegui;
+package edu.sjsu.cs151;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
