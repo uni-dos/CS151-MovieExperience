06d3e459d29e4ab4153430bf6d33d361a77b771f
uni-dos <uni.dos12@outlook.com>
2023-11-13 11:25:12 -0800

    integrate network calls to gui

--- src/main/java/edu/sjsu/cs151/MovieExperienceApplication.java
+++ src/main/java/edu/sjsu/cs151/MovieExperienceApplication.java
@@ -8,27 +8,32 @@ import javafx.scene.Parent;
 
 import java.io.IOException;
 
+import static com.sun.javafx.application.PlatformImpl.exit;
+
 public class MovieExperienceApplication extends Application {
 
     @Override
-    public void start (Stage stage) throws IOException
+    public void start (Stage stage)
     {
         try
         {
             Parent root = FXMLLoader.load(getClass().getResource("homeView.fxml"));
-            FXMLLoader fxmlLoader = new FXMLLoader(MovieExperienceApplication.class.getResource("homeView.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+            Scene scene = new Scene(root, 600, 400);
             stage.setTitle("Movie Experience");
             stage.setScene(scene);
+            stage.setResizable(false);
             stage.show();
         }
         catch(Exception e)
         {
             e.printStackTrace();
         }
+
     }
 
     public static void main(String[] args) {
         launch();
+        // need this or else the retrofit instance keeps running
+        System.exit(0);
     }
 }
